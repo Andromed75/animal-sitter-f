9d43de534279c072c2e24419c1888c189a24a2e8
Arthur <44750598+Andromed75@users.noreply.github.com>
2020-04-03 14:57:58 +0200

    WIP : pb favico

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [LayoutComponent, NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [LayoutComponent]
 })
